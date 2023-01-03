805d0f96044e5bec296970552dc618bb33b06573
LatekVon <latkaignacy@gmail.com>
2023-01-03 01:23:07 +0100

    - added user-data / login-box depending on user's login status - added function getPositionById - added API reference to the main.js - fixed board's layout depending on player pieces' color - partially finished graphical moving functionality - roughly / partially completed the following functions and api calls: * /createGame * /joinGame
    Graphical implementation of these functions is still pending, but will be currently a topmost priority

--- client/main.js
+++ client/main.js
@@ -10,19 +10,104 @@ let localUserId = undefined
 let initPos_x, initPos_y
 let isClicked = false
 
-// get clicked field, and highlight it
-function moveStart() {
+function getPositionById(id) {
+    let domObject = document.getElementById(id);
 
+    let x = 0, y = 0
+    while (domObject !== null){
+        x += domObject.offsetLeft;
+        y += domObject.offsetTop;
+
+        domObject = domObject.offsetParent;
+    }
+
+    return [x, y];
+}
+
+// get clicked field, and highlight it
+function moveStart(e) {
+    initPos_x = e.clientX
+    initPos_y = e.clientY
+
+    isClicked = true
 }
 
 // get other field, and move the piece there
-function moveEnd() {
+function moveEnd(e, startPos_x, startPos_y, endPos_x, endPos_y) {
+    // based on field size, get both the selected chess fields and move the piece from one to another
+
+    // TODO: iterate differently depending on your color
+
+    let x_pos = undefined, y_pos = undefined // html id
+    let x_real = undefined, y_real = undefined // array coords
+
+    for (let x = 0; x < 8; x++) {
+
+        x_pos = x + 1
+    }
+
+    for (let y = 0; y < 8; y++) {
+        y_pos = String.fromCharCode('a'.charCodeAt(0) + y)
+    }
+
+    // just in case
+    if (x_pos === undefined || y_pos === undefined) {
+        isClicked = false
+        return
+    }
+
+    // move the img and submit the move
+
 
 }
 
-function addClickListener() {
+// only checks for bounds, then passes all data along accordingly to the current state of 'isClicked'
+function boardClickListener(e) {
+    // get field's size & mouse pos, compare them
+    // cancel move (isClicked = false) if it's the same square
+    let mousePos_x = e.clientX,
+        mousePos_y = e.clientY
 
+    // get position of the 0,0 chess field, and the 1,1 chess field, to calculate a single field's size
+
+    let fieldPosA, fieldPosB
+
+    if (localPieceColor === pe.WHITE) {
+        fieldPosA = getPositionById("a8")
+        fieldPosB = getPositionById("b7")
+    } else {
+        fieldPosA = getPositionById("h1")
+        fieldPosB = getPositionById("g2")
+    }
+
+    let pos_start_x = fieldPosA[0]
+    let pos_start_y = fieldPosA[1]
+
+    let fieldSize = fieldPosB[0] - fieldPosA[0]
+
+    let rbCorner_x = fieldPosA[0] + fieldSize * 8,
+        rbCorner_y = fieldPosA[1] + fieldSize * 8
+
+    // note: for y, higher on page = lower number
+
+    // check for out of bounds clicks
+    if (mousePos_x < pos_start_x || mousePos_x > rbCorner_x ||
+        mousePos_y < pos_start_y || mousePos_y > rbCorner_y) {
+        isClicked = false
+        return
+    }
+
+    console.log('click!')
+    console.log(e.clientY)
+
+    if (isClicked)
+        moveEnd(e)
+    else
+        moveStart(e)
 }
+
+// clicking anywhere on the doc will (should) check for mouse pos
+document.onmousedown = boardClickListener
 
 function drawBoard() {
 
@@ -33,12 +118,29 @@ function drawBoard() {
         html_end_cell = '">'
 
     document.getElementById('game-board').innerHTML = ""
-    for (let ver = 0; ver < 8; ver++) {
 
-        let ver_real = String(ver + 1);
-        document.getElementById('game-board').innerHTML += (html_new_row + ver_real + html_end_row);
+    for (let v_it = 0; v_it < 8; v_it++) {
 
-        for (let hor = 0; hor < 8; hor++) {
+        let ver = v_it
+
+        if (localPieceColor === pe.WHITE)
+            ver = 7 - v_it
+
+        // real - in the array
+        // visual - on the html board
+
+        let ver_visual = String(ver + 1);
+        document.getElementById('game-board').innerHTML += (html_new_row + ver_visual + html_end_row);
+
+        for (let h_it = 0; h_it < 8; h_it++) {
+
+            let hor = h_it
+
+            if (localPieceColor === pe.BLACK)
+                hor = 7 - h_it
+
+            let hor_visual = String.fromCharCode('a'.charCodeAt(0) + hor)
+
             let pieceId = localPlayingField[ver][hor]
 
             let filename = undefined
@@ -93,13 +195,12 @@ function drawBoard() {
             let html_piece_end_img = '">'
 
 
-            let hor_real = String.fromCharCode('a'.charCodeAt(0) + hor)
-            document.getElementById(ver_real).innerHTML += (html_new_cell + hor_real + ver_real + html_end_cell);
-            let id = ver_real + hor_real;
+            document.getElementById(ver_visual).innerHTML += (html_new_cell + hor_visual + ver_visual + html_end_cell);
+            let id = ver_visual + hor_visual;
 
             if (rawFilename !== undefined) {
-                console.log('loading texture to: ' + hor_real + ver_real)
-                document.getElementById(hor_real + ver_real).innerHTML += (html_piece_new_img + filename + html_piece_end_img)
+                console.log('loading texture to: ' + hor_visual + ver_visual)
+                document.getElementById(hor_visual + ver_visual).innerHTML += (html_piece_new_img + filename + html_piece_end_img)
 
             }
             console.log(id);
@@ -112,21 +213,6 @@ function reDownloadBoard() {
     let requestForBoard = new XMLHttpRequest();
 }
 */
-
-// source: https://stackoverflow.com/questions/1769584/get-position-of-element-by-javascript
-function getPositionById(id){
-    let domObject = document.getElementById(id);
-
-    let x = 0, y = 0
-    while (domObject !== null){
-        x += domObject.offsetLeft;
-        y += domObject.offsetTop;
-
-        domObject = domObject.offsetParent;
-    }
-
-    return [x, y];
-}
 
 /* IMPLEMENTATION WILL BE SUSPENDED FOR THE TIME BEING
 let anonymousLoginRequest = new XMLHttpRequest();
@@ -142,24 +228,52 @@ joinGameRequest.onreadystatechange = function () {
 
 };
 
+// POST /joinGame {userId, boardId}
 function joinGame() {
+    let body = new FormData()
+    body.set('userId', localUserId)
+    body.set('boardId', localBoardId)
 
+    createGameRequest.open("GET", "/joinGame", true)
+    createGameRequest.send(body)
 }
 
 let createGameRequest = new XMLHttpRequest();
 
 createGameRequest.onreadystatechange = function () {
+    let rawData = JSON.parse(createGameRequest.responseText);
 
+    localBoardId = rawData["boardId"]
 };
 
+// POST /createGame {userId}
 function createGame() {
-    postsRequest.open("GET", "/createBoard", true);
-    postsRequest.send();
+    let body = new FormData()
+    body.set('userId', localUserId)
+
+    createGameRequest.open("GET", "/createBoard", true)
+    createGameRequest.send(body)
 }
 
 function drawLoginInfo() {
-    let loginInfoNotLoggedInPage = ''
-    let loginInfoLoggedInPage = ''
+    let loginInfoNotLoggedInPage =
+        '<a href="register.html" class="flex-container">' +
+        '    <button id="go-register" class="flex-item menu-button">REGISTER</button>' +
+        '</a>' +
+        '<a href="login.html" class="flex-container">' +
+        '    <button id="go-login" class="flex-item menu-button">LOGIN</button>' +
+        '</a>'
+
+    let loginInfoLoggedInPage =
+        '<div id="user-details" class="flex-item">Username: [USERNAME]<br>Current match: [BOARD_ID]<br>Current opponent: [OPP_USERNAME]<br></div>' +
+        '<button id="go-logout" class="flex-item menu-button">LOGOUT</button>'
+
+    if (localUserId === undefined) {
+        document.getElementById('user-data').innerHTML = loginInfoNotLoggedInPage
+    } else {
+        // todo: fill in the blanks
+        document.getElementById('user-data').innerHTML = loginInfoLoggedInPage
+    }
 
 }
 
@@ -169,10 +283,10 @@ API LIST:
  - GET /
  - POST /login (handled by html form)
  - POST /register (handled by html form)
- - POST /createGame
- - POST /joinGame
- - POST /getMove
- - POST /makeMove
+ - POST /createGame {userId}
+ - POST /joinGame {userId, boardId}
+ - POST /getMove {userId}
+ - POST /makeMove {userId, moveFrom, moveTo} (moveFrom/moveTo = {x, y})
 
 CLIENT - new request
 let postsRequest = new XMLHttpRequest();
@@ -214,7 +328,7 @@ QUICK EXPLANATION:
 makeDraggable(document.getElementById("mydiv"));
 
 function makeDraggable(element) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(element.id + "header")) {
     // if present, the header is where you move the DIV from:
     document.getElementById(element.id + "header").onmousedown = dragMouseDown;
